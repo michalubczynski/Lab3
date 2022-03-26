c9ca9476a31eefaca638ea552a31b0195bb6cee4
michalubczynski <michlub322@student.polsl.pl>
2022-03-26 18:24:16 +0100

    Upload final version

---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Motoryzacja;
+using System.Threading;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void WyswietlPojazdy(Pojazd[] l)
+        {
+            foreach (var item in l)
+            {
+                lstBoxZadA.Items.Add(item);
+            }
+        }
+
+        private async void btnZadanieA_Click(object sender, RoutedEventArgs e)
+        {
+            btnZadanieA.IsEnabled = false;
+            try
+            {
+
+                lstBoxZadA.Items.Clear();
+                Pojazd[] p = new Pojazd[5];
+                p[0] = new Pojazd("Skuter", 2, 50);
+                p[1] = new Pojazd("Quad", 20);
+                p[2] = new Pojazd() { Nazwa = "Tesla", Prędkość = 100, LiczbaKół = 2 };
+                p[3] = new PojazdMechaniczny("Ciagnik", 4, 15, 60);
+                p[4] = new Samochod("Alfina", 140, 140, "Alfa Romero", 4);
+                //p[5] = new Samochod("Alfina", 140, 140, "Alfa Romero", 0); // Example of exceptions 
+                WyswietlPojazdy(p);
+                await Task.Delay(2000);
+                this.lstBoxZadA.Items.Add("History of changes:");
+                p[0].Nazwa = "NowaNazwa";
+                p[0].WyswietlHistorie(ref lstBoxZadA);
+            }
+            catch (ArgumentException) {
+                MessageBox.Show("Incorrect value in vehicle argument");
+            }
+            
+            
+        }
+    }
+
+}
+//Dodajemy bibliotekę klas aplikacji WPF .net framework 
+//Dodajemy w LAB3 odwołanie do Motoryzacji
+//Dyrektywa Using Motoryzacja
---
+++ Motoryzacja/Pojazd.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Motoryzacja
+{
+    struct RejestrNazw
+    {
+        DateTime datamodyfikacji { get; set; }
+        string Nazwa { get; set; }
+        public RejestrNazw(string nazwa, DateTime data)
+        {
+            this.Nazwa = nazwa;
+            this.datamodyfikacji = data;
+        }
+        public override string ToString() {
+            return this.Nazwa + this.datamodyfikacji;
+        }
+    }
+    public class Pojazd
+    {
+        private string nazwa;
+        public string Nazwa { get { return this.nazwa; } set { this.nazwa = value; historiaNazw.Add(new RejestrNazw(this.nazwa, DateTime.Now));  } }
+        private int liczbaKół;
+        public int LiczbaKół { get { return liczbaKół; } set { if (value < 1 ) { throw new ArgumentException(); } else liczbaKół = value; } }
+        private double prędkość;
+        public double Prędkość { get { return prędkość; } set { if (value < 0) { throw new ArgumentException(); } else prędkość = value; } }
+        public int Lp { get; private set; }
+        static private int liczbaPojazdow;
+        private List<RejestrNazw> historiaNazw = new List<RejestrNazw>();
+
+         public void WyswietlHistorie(ref ListBox l) { //Dodać do klasy Pojazd metodę WyświetlHistorię przyjmującą jako parametr referencję do listy ListBox, na której chcemy zobaczyć historię zmian nazwy
+            foreach (RejestrNazw rejestrNazw in this.historiaNazw) { 
+            l.Items.Add(rejestrNazw);
+            }
+         }
+
+
+        static Pojazd() {
+            liczbaPojazdow = 0;
+        }
+        
+        public Pojazd (string nazwa, int kola, double predkosc):this()
+        {
+            Nazwa = nazwa;
+            LiczbaKół= kola;
+            Prędkość= predkosc;
+        }
+        public Pojazd(string nazwa, double predkosc) : this(nazwa, 4, predkosc)
+        {
+            Nazwa= nazwa;
+            Prędkość = predkosc;
+        }
+        public Pojazd() {
+            ++liczbaPojazdow;
+            this.Lp = liczbaPojazdow;
+        }
+ 
+        
+        public override string ToString() {
+            return "Lp."+this.Lp+"/"+Pojazd.liczbaPojazdow+". "+this.Nazwa + ", Kół:" + this.LiczbaKół + " Prędkość:" + this.Prędkość;
+        }
+
+    }
+    public class PojazdMechaniczny : Pojazd
+    {
+        public int MocSilnika { get { return this.mocSilnika; } set { if (value > 0) mocSilnika = value; else throw new ArgumentException(); } }
+        public int mocSilnika;
+        public PojazdMechaniczny(string nazwa, int ilKol, double predkosc, int mocSilnika) : base(nazwa, ilKol, predkosc)
+        {
+            MocSilnika = mocSilnika;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + ", moc silnika:" + this.mocSilnika;
+        }
+    }
+    public sealed class Samochod : PojazdMechaniczny
+    {
+        public string Marka { get; set; }
+        public int liczbaPasazerow;
+        public int LiczbaPasazerow { get { return liczbaPasazerow; } set { if (value < 1) { throw new ArgumentException(); } else liczbaPasazerow = value; } }
+        public Samochod(string m_nazwa, double predkosc, int moc, string marka, int pasazery) : base(m_nazwa, 4, predkosc, moc)
+        {
+            LiczbaPasazerow = pasazery;
+            Marka = marka;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + ", Marka:" + Marka + ", Pasażerów:" + LiczbaPasazerow;
+        }
+    }
+}
